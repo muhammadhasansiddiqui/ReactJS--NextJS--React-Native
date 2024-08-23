59fd50e6f4f5a6bca4a80fee8067d49e752fb188
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-24 00:28:36 +0500

    props

--- props and component/src/Components/Avatar.jsx
+++ props and component/src/Components/Avatar.jsx
@@ -1,19 +1,39 @@
-function UserData( {name , capitalCity, img }) {
-    return (
-<div>
+import React from 'react';
+import Swal from 'sweetalert2';
 
+function UserData({ name, capitalCity, img, city }) {
+  const handleClick = () => {
+    Swal.fire({
+      title: 'City Information',
+      text: `City: ${capitalCity}`,
+      icon: 'info',
+      confirmButtonText: 'OK',
+      customClass: {
+        container: 'custom-alert',
+        title: 'custom-alert-title',
+        content: 'custom-alert-content',
+        text: 'custom-alert-text',
+        confirmButton: 'custom-alert-confirm-button',
+        
+      }
+    });
+  };
 
-<div>
+  return (
+    <div>
+      <div>
+        <h1 className="font-mono text-3xl text-yellow-200">{name}</h1>
+        <h3 className="text-zinc-100">{capitalCity}</h3>
+      </div>
+      <img
+        className=""
+        style={{ objectFit: 'contain', width: '100%', height: '200px', cursor: 'pointer' }}
+        onClick={handleClick}
+        src={img}
+        alt={name}
+      />
+    </div>
+  );
+}
 
-<h1 className="text-3xl font-bold underline"  > {name} </h1>
-<h3>{capitalCity}</h3>
-
-</div>
-<img src ={img} />
-</div>
-
-    )
-    }
-
-
-    export default UserData
+export default UserData;
