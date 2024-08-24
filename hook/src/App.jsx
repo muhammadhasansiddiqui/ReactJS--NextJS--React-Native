62c8a9aba0230eda58394e72bd70de7aba63b8aa
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-25 02:47:47 +0500

    Hook

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -4,18 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [userName, setUserName] = useState ("hasan");
-  const [count, setCount] = useState(0);
+  // const [userName, setUserName] = useState ("hasan");
+  // const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+  // function handleClick() {
+  //   setCount(count + 1);
+  // }
+  // use with input
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
 
   return (
     <div>
-   <h1>Hi am {userName}</h1>
-   <button onClick={handleClick}>Click Me</button>
-   <p>Count: {count}</p>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+
+    {/* <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
+
+   <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
+   <h1>Hi am {userName}</h1>  */}
+   {/* <button onClick={handleClick}>Click Me</button>
+   <p>Count: {count}</p> */}
 
 
     </div>
