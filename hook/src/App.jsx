277b2b88ef83975f37ee192a67882fc754109774
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-25 02:39:47 +0500

    hook

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -5,10 +5,17 @@ import './App.css'
 
 function App() {
   const [userName, setUserName] = useState ("hasan");
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
 
   return (
     <div>
    <h1>Hi am {userName}</h1>
+   <button onClick={handleClick}>Click Me</button>
+   <p>Count: {count}</p>
 
 
     </div>
