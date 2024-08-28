7f13de89bdf481873da714b130523bc385d91113
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-28 21:58:12 +0500

    work

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -2,54 +2,49 @@ import { useEffect, useState } from "react";
 import './App.css';
 
 function UseEffect() {
-  const [cars, setCars] = useState([]);
+  const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
     console.log("useEffect is called");
-    getCARS();
+    getProducts();
   }, []);
 
-  const getCARS = async () => {
-    try {
-      const response = await fetch("https://freetestapi.com/api/v1/cars");
-      const data = await response.json();
-      setCars(data);
-      console.log("🚀 ~ getCARS ~ data:", data);
-    } catch (error) {
-      console.error("Failed to fetch cars:", error);
-    }
+
+  const getProducts = () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("res->", res);
+        setProducts(res);
+      });
   };
 
+ 
+
   return (
-    <div>
-      <h1>Cars</h1>
-
-      <h1>Search</h1>
-      <input
-        type="text"
-        placeholder="Search"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      
-        {cars
-          .filter(car => car.make.toLowerCase().includes(search.toLowerCase()))
-          .map(car => (
-            <div key={car.id}>
-              <img src={car.image} alt={`${car.make} ${car.model}`} />
-              <h2>{car.make}</h2>
-              <p>{car.model}</p>
-              <p>{car.year}</p>
+    <div className="container flex flex-col justify-center items-center  mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Products</h1>
+  
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {products.map(product => (
+          <div key={product.id} className="border rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-200">
+            <img src={product.image} alt="Product Img" className="w-full h-10  object-contain rounded-t-lg mb-4" />
+            <h1 className="text-xl font-semibold mb-2"> {product.title} </h1>
+            <h2 className="text-lg text-gray-700 mb-2"> ${product.price} </h2>
+            <p className="text-sm text-gray-600 mb-4"> {product.description} </p>
+            <p className="text-sm text-gray-500 mb-2">Category: {product.category} </p>
+            <div className="flex items-center justify-between text-sm text-gray-500">
+              <span>Rating: {product.rating.rate}</span>
+              <span>{product.rating.count} Reviews</span>
             </div>
-          ))}
-        
-
-      <h1>Fetch</h1>
-      <button onClick={getCARS}>Fetch</button>
+            <hr className="mt-4" />
+          </div>
+        ))}
+      </div>
     </div>
   );
+  
 }
 
 export default UseEffect;
