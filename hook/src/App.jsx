cfbdc141c32a220b1d8aec2baae84d257ffdfaa3
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-30 20:04:35 +0500

    hook

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -8,27 +8,12 @@ function App() {
 
   return (
     <div className="max-w-md p-4 mx-auto">
-<h1 className="px-4 py-2 mr-2 font-semibold text-pink-500  rounded text-lg ">Todos</h1>
-<div>
 
-<input type="text"
-placeholder="Add todo" value={newTodo} className= "border border-gray-400 rounded-md px-4 py-2 mr-2" />
-<button className="px-4 py-2 mr-2 font-semibold text-white bg-pink-500 rounded" 
-onChange={(e) => {
-  setTodos([...todos, newTodo]);
-  setNewTodo("");
-}}
->Add</button>
-
-<div>
-<ul></ul>
-
-</div>
 
 </div>
       
      
-    </div>
+    
   );
 }
 
