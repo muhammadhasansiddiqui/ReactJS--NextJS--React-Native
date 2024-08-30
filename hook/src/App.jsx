f63542ea825585678aa6551337a1f9aa2d303993
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-08-30 21:45:10 +0500

    hook

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 
-
-
 function App() {
-  
+  const [name, setName] = useState("Hasan");
 
   return (
-    <div className="max-w-md p-4 mx-auto">
-
-
-</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="bg-yellow-300 mb-4 px-4 py-2">{name}</h1>
       
-     
-    
+      <button
+        onClick={() => setName("Bahi")}
+        className="px-4 py-2 mb-2 bg-blue-500 text-white rounded"
+      >
+        Update
+      </button>
+      
+      <button
+        onClick={() => setName("Hasan")}
+        className="px-4 py-2 bg-red-500 text-white rounded"
+      >
+        RE TURN
+      </button>
+    </div>
   );
 }
 
