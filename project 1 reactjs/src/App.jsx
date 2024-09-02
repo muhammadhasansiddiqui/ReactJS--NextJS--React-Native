06d5ab7d795bc10c3e76f11955399354d7a68204
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-09-02 22:59:59 +0500

    hook

--- project 1 reactjs/src/App.jsx
+++ project 1 reactjs/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState ,useRef } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function UseState() {
-  const inputRef = useRef()
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-     <h1>{inputRef}</h1>
-     console.log("🚀 ~ UseState ~ inputRef:", inputRef);
-
-
+      <h1 className="from-neutral-50">{count}</h1>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>inc</button>
+      <button disabled={count < 1}  onClick={() => setCount(prevCount => prevCount - 1)}>dec</button>
     </div>
   );
 }
